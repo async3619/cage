07f98fb9e6d8db32564afd5125cc6b9aaf16efe1
Sophia <beyondthesophia@gmail.com>
2022-11-30 17:13:57 +0900

    refactor(core): add basic watching feature through cron

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as chalk from "chalk";
 import * as fs from "fs-extra";
+import * as cron from "node-cron";
+import cronstrue from "cronstrue";
 
 import { AVAILABLE_PROVIDERS } from "@watchers";
 
@@ -37,27 +39,62 @@ async function main() {
         });
     }
 
-    for (const provider of AVAILABLE_PROVIDERS) {
-        await Logger.work("info", `finalize \`${chalk.green(provider.getName())}\` watcher ... `, async () => {
-            await provider.finalize();
-        });
-    }
+    const providerNames = AVAILABLE_PROVIDERS.map(p => `\`${chalk.green(p.getName())}\``).join(", ");
+    const formattedCron = cronstrue.toString("0 * * * * *");
 
-    for (const provider of AVAILABLE_PROVIDERS) {
-        await Logger.work(
-            "debug",
-            `saving serialized data of \`${chalk.green(provider.getName())}\` watcher ... `,
-            async () => {
-                const data = provider.serialize();
-                const filePath = `./dump/${provider.getName().toLowerCase()}.json`;
+    Logger.info(`start to watch through ${providerNames} provider${AVAILABLE_PROVIDERS.length === 1 ? "" : "s"}:`);
+    Logger.info(` - watching cron rule: \`0 * * * * *\` ${chalk.gray(`(${formattedCron})`)}`);
 
-                await fs.ensureFile(filePath);
-                await fs.writeJson(filePath, data, { spaces: 4 });
-            },
-        );
-    }
+    const task = cron.schedule("0 * * * * *", async () => {
+        for (const provider of AVAILABLE_PROVIDERS) {
+            await Logger.work(
+                "info",
+                `determine followers data through \`${chalk.green(provider.getName())}\` watcher ... `,
+                async () => {
+                    await provider.doWatch();
+                },
+            );
+        }
+    });
 
-    Logger.info("Bye World from Cage! 🐦");
+    let cleaningUp = false;
+    const cleanUp = async () => {
+        if (cleaningUp) {
+            return;
+        }
+
+        cleaningUp = true;
+        task.stop();
+
+        for (const provider of AVAILABLE_PROVIDERS) {
+            await Logger.work("info", `finalize \`${chalk.green(provider.getName())}\` watcher ... `, async () => {
+                await provider.finalize();
+            });
+        }
+
+        for (const provider of AVAILABLE_PROVIDERS) {
+            await Logger.work(
+                "debug",
+                `saving serialized data of \`${chalk.green(provider.getName())}\` watcher ... `,
+                async () => {
+                    const data = provider.serialize();
+                    const filePath = `./dump/${provider.getName().toLowerCase()}.json`;
+
+                    await fs.ensureFile(filePath);
+                    await fs.writeJson(filePath, data, { spaces: 4 });
+                },
+            );
+        }
+
+        Logger.info("Bye World from Cage! 🐦");
+    };
+
+    process.on("exit", cleanUp);
+    process.on("SIGTERM", cleanUp);
+    process.on("SIGINT", cleanUp);
+    process.on("SIGUSR1", cleanUp);
+    process.on("SIGUSR2", cleanUp);
+    process.on("uncaughtException", cleanUp);
 }
 
 main();
