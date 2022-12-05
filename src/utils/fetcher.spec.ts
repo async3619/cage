cefe007f194b0468e753d18dc3367cae7f86b027
Sophia <me@sophia-dev.io>
2022-12-05 21:23:01 +0900

    test: fix logger printed value expectation

--- src/utils/fetcher.spec.ts
+++ src/utils/fetcher.spec.ts
@@ -167,7 +167,7 @@ describe("Fetcher class", function () {
         });
 
         await expect(target.fetch({ url: "", retryCount: 3, retryDelay: 0 })).rejects.toThrow(
-            "Failed to fetch : (500 Internal Server Error)",
+            "500 (Internal Server Error)",
         );
         expect(calledCount).toBe(4);
     });
@@ -189,7 +189,7 @@ describe("Fetcher class", function () {
 
         const [, elapsedTime] = await throttle(
             expect(target.fetch({ url: "", retryCount: 3, retryDelay: 500 })).rejects.toThrow(
-                "Failed to fetch : (500 Internal Server Error)",
+                "500 (Internal Server Error)",
             ),
             0,
             true,
